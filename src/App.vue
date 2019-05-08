b57b194b4b9c7e89bec017a2da4df5b539748abb
kromchenko <kromchenko1987@gmail.com>
2019-05-08 14:53:58 +0300

    new vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
@@ -11,18 +10,18 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     HelloWorld
+  },
+  data: function () {
+    return {
+      msg: "Test String!",
+      year: "2019"
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
