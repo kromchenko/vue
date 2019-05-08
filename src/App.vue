ad496a84c6ae434335d7f50b520cad318387db73
kromchenko <kromchenko1987@gmail.com>
2019-05-08 15:52:14 +0300

    Add new task

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id"> {{ task.title }} </li>
+    </ul>
+    <form action="" @submit.prevent="addNewTask">
+      <input type="text">
+      <button>Add task</button>
+    </form>
   </div>
 </template>
 
@@ -15,8 +22,18 @@ export default {
   },
   data: function () {
     return {
-      msg: "Test String!",
-      year: "2019"
+      msg: "Task list",
+      tasks: [
+        {id:1, title: "Get up"},
+        {id:2, title: "Never give up"},
+        {id:3, title: "Victory"},
+      ]
+    }
+  },
+  methods:{
+    addNewTask(e){
+      console.log(e.target[0].value);
+      this.tasks.push({id:this.tasks.length +1, title: e.target[0].value})
     }
   }
 }
